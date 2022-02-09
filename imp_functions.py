76a10d48fa1a35682189318c9b9034c100f5fc9a
Shivam <shivam09052000@gmail.com>
2022-02-09 20:59:14 +0530

    done minor changes

--- imp_functions.py
+++ imp_functions.py
@@ -10,17 +10,17 @@ def deleting(id):
     import glob
     id = str(id)
     try:
-        for i in glob.glob('music'+id+'/*'):
+        for i in glob.glob(f'music{id}/*'):
             os.remove(i)
 
-        if os.path.exists('music'+id):
-            os.rmdir('music'+id)
+        if os.path.exists(f'music{id}'):
+            os.rmdir(f'music{id}')
     except Exception as e:
         print(e)
     try:
-        if os.path.exists('playlist'+id+'.zip'):
+        if os.path.exists(f'playlist{id}.zip'):
 
-            os.remove('playlist'+id+'.zip')
+            os.remove(f'playlist{id}.zip')
     except Exception as e:
         print(e)
 
@@ -28,8 +28,8 @@ def deleting(id):
 def create_directory(id):
     import os
     id = str(id)
-    if not os.path.exists('music'+id):
-        os.makedirs('music'+id)
+    if not os.path.exists(f'music{id}'):
+        os.makedirs(f'music{id}')
 
 
 def replacesomthing(text):
@@ -71,10 +71,8 @@ def analayse_type_of_link_of_youtube(link):
     import re
     if re.search('&list', link):
         return 'yt-playlist'
-    elif re.search('watch?v=', link):
-        return 'yt-track'
     else:
-        return 'error'
+        return 'yt-track'
 
 
 def analyse_link_of_spotify_or_youtube(link):
